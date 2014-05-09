e07f5e5f0ceead238f29d4f1b120f3b81ded0a82
William Getz <willegetz@gmail.com>
2014-05-08 23:49:08 -0700

    Added additional properties.

--- ObsidianPortal.Owin/Providers/ObsidianPortalAuthenticatedContext.cs
+++ ObsidianPortal.Owin/Providers/ObsidianPortalAuthenticatedContext.cs
@@ -1,13 +1,30 @@
 ﻿using System.Security.Claims;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Provider;
 
 namespace NoTPK.Owin.Security.ObsidianPortal.Providers
 {
-	public class ObsidianPortalAuthenticatedContext
+	public class ObsidianPortalAuthenticatedContext : BaseContext
 	{
+		public ObsidianPortalAuthenticatedContext(IOwinContext context, string userId, string screenName, string accessToken, string accessTokenSecret) : base(context)
+		{
+			UserId = userId;
+			ScreenName = screenName;
+			AccessToken = accessToken;
+			AccessTokenSecret = accessTokenSecret;
+		}
+
+		public string UserId { get; set; }
+
+		public string ScreenName { get; set; }
+
 		public string AccessToken { get; set; }
 
 		public string AccessTokenSecret { get; set; }
 
 		public ClaimsIdentity Identity { get; set; }
+		
+		public AuthenticationProperties Properties { get; set; }
 	}
 }
