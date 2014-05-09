8f50f7489aa0e1179a3d824b390b6673853e1694
William Getz <willegetz@gmail.com>
2014-05-09 00:21:51 -0700

    Completed provider work

--- ObsidianPortal.Owin/Providers/ObsidianPortalReturnEndpointContext.cs
+++ ObsidianPortal.Owin/Providers/ObsidianPortalReturnEndpointContext.cs
@@ -1,7 +1,12 @@
-﻿namespace NoTPK.Owin.Security.ObsidianPortal.Providers
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Provider;
+
+namespace NoTPK.Owin.Security.ObsidianPortal.Providers
 {
-	public class ObsidianPortalReturnEndpointContext
+	public class ObsidianPortalReturnEndpointContext : ReturnEndpointContext
 	{
-		 
+		public ObsidianPortalReturnEndpointContext(IOwinContext context, AuthenticationTicket ticket) : base(context, ticket)
+		{ }
 	}
 }
